ec92700088813a883c6aacf919ee452b182a66e9
pc-user <pc-user@pc-user-PC>
2018-01-22 11:40:31 +0900

    課題0115

--- src/menu/Menu_main.java
+++ src/menu/Menu_main.java
@@ -3,9 +3,9 @@ package menu;
 public class Menu_main {
 
 	public static void main(String[] args) {
-		Menu_view frame = new Menu_view();
-		frame.setBounds(5, 5, 605, 455);
-		frame.setVisible(true);
+		Menu_view view = new Menu_view();
+		view.setBounds(5, 5, 755, 475);
+		view.setVisible(true);
 
 	}
 
--- src/menu/Menu_view.java
+++ src/menu/Menu_view.java
@@ -1,7 +1,17 @@
 package menu;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -9,70 +19,67 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-
 public class Menu_view extends Frame implements ActionListener, WindowListener{
+	
 	private MenuBar mb;
 	private Menu graph;
 	private MenuItem graph1, graph2;
 	private Panel cardPanel, card1, card2;
 	
 	public Menu_view() {
-		super();
-		// TODO Auto-generated constructor stub
 		addWindowListener(this);
 		setTitle("Menu");
+		//メニューの作成
 		mb = new MenuBar();
 		graph = mb.add(new Menu("Graph"));
 		graph1 = graph.add(new MenuItem("Graph1"));
 		graph2 = graph.add(new MenuItem("Graph2"));
 		setMenuBar(mb);
-		graph1.addActionListener(this);
-		graph2.addActionListener(this);
 		
 		cardPanel = new Panel();
 		cardPanel.setLayout(new CardLayout());
-		add(cardPanel);
-		
 		card1 = new Panel();
+		card2 = new Panel();
+		//card1にグラフの設定
 		DefaultCategoryDataset data1 = new DefaultCategoryDataset();
-		data1.addValue(300, "USA", "2015");
-		data1.addValue(100, "USA", "2016");
-		data1.addValue(200, "USA", "2017");
-		JFreeChart chart1 =
-			ChartFactory.createLineChart(
-					"Import Volume",
-					"Year",
-					"Ton",
-					data1,
-					PlotOrientation.VERTICAL,
-					true,
-					false,
-					false
-		);
+		data1.addValue(300, "USA", "2005");
+		data1.addValue(500, "USA", "2006");
+		data1.addValue(120, "USA", "2007");
+		JFreeChart chart1 = 
+				ChartFactory.createLineChart("Import Volume",
+						"Year",
+						"Ton",
+						data1,
+						PlotOrientation.VERTICAL,
+						true,
+						false,
+						false);
 		ChartPanel cpanel1 = new ChartPanel(chart1);
 		card1.add(cpanel1, BorderLayout.CENTER);
-		
-		card2 = new Panel();
+		//card2にグラフの設定
 		DefaultCategoryDataset data2 = new DefaultCategoryDataset();
-		data2.addValue(100, "China", "2015");
-		data2.addValue(300, "China", "2016");
-		data2.addValue(150, "China", "2017");
-		JFreeChart chart2 =
-			ChartFactory.createLineChart(
-					"Import Volume",
-					"Year",
-					"Ton",
-					data2,
-					PlotOrientation.VERTICAL,
-					true,
-					false,
-					false
-		);
+		data2.addValue(200, "China", "2005");
+		data2.addValue(100, "China", "2006");
+		data2.addValue(320, "China", "2007");
+		JFreeChart chart2 = 
+				ChartFactory.createLineChart("Import Volume",
+						"Year",
+						"Ton",
+						data2,
+						PlotOrientation.VERTICAL,
+						true,
+						false,
+						false);
 		ChartPanel cpanel2 = new ChartPanel(chart2);
 		card2.add(cpanel2, BorderLayout.CENTER);
-		
+		//カードを反映
 		cardPanel.add(card1, "Card1");
 		cardPanel.add(card2, "Card2");
+		add(cardPanel);
+		//Listenerの設定
+		graph1.addActionListener(this);
+		graph2.addActionListener(this);
+		
 	}
 
 	@Override
@@ -85,7 +92,6 @@ public class Menu_view extends Frame implements ActionListener, WindowListener{
 	public void windowClosing(WindowEvent e) {
 		// TODO Auto-generated method stub
 		System.exit(0);
-		
 	}
 
 	@Override
@@ -121,14 +127,15 @@ public class Menu_view extends Frame implements ActionListener, WindowListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		CardLayout cardlayout = (CardLayout)(cardPanel.getLayout());
-		if (e.getSource() == graph1) {
+		CardLayout cardLayout = (CardLayout)(cardPanel.getLayout());
+		if(e.getSource() == graph1) {
 			setBackground(Color.red);
-			cardlayout.show(cardPanel, "Card1");
+			cardLayout.show(cardPanel, "Card1");
 		}
-		if (e.getSource() == graph2) {
+		if(e.getSource() == graph2) {
 			setBackground(Color.blue);
-			cardlayout.show(cardPanel, "Card2");
+			cardLayout.show(cardPanel, "Card2");
 		}
 	}
+
 }
